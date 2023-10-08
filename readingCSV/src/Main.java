2645d0bb85e1f0bd13c5ea713bcacb9a1b31d98f
Abhijeet103 <93581505+Abhijeet103@users.noreply.github.com>
2023-10-08 12:39:17 +0530

    Add files via upload

--- readingCSV/src/Main.java
+++ readingCSV/src/Main.java
@@ -1,10 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+
 import entities.*;
 
 public class Main {
@@ -15,6 +17,23 @@ public class Main {
         return hrs + (mins / 60.0);
     }
 
+    public static String convert12To24(String time12hr) {
+        if (time12hr == null)
+        {
+            return null;
+        }
+        try {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("hh:mm a");
+            SimpleDateFormat outputFormat = new SimpleDateFormat("HH:mm");
+            Date date = inputFormat.parse(time12hr);
+            return outputFormat.format(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null; // Handle the exception as needed
+        }
+    }
+
+
     public static long calculateDateDifference(String startDateString, String endDateString) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
@@ -23,8 +42,41 @@ public class Main {
 
         return java.time.temporal.ChronoUnit.DAYS.between(startDate, endDate);
     }
+
+    public static long manual_hours_diffrence(String date1, String date2) {
+        // Extract hours, minutes, and AM/PM from time strings
+        int hours1 = Integer.parseInt(date1.substring(11, 13));
+        int minutes1 = Integer.parseInt(date1.substring(14, 16));
+        String ampm1 = date1.substring(17, 19);
+
+        int hours2 = Integer.parseInt(date2.substring(11, 13));
+        int minutes2 = Integer.parseInt(date2.substring(14, 16));
+        String ampm2 = date2.substring(17, 19);
+
+        // Convert to 24-hour format
+        if (ampm1.equalsIgnoreCase("PM") && hours1 != 12) {
+            hours1 += 12;
+        }
+        if (ampm2.equalsIgnoreCase("PM") && hours2 != 12) {
+            hours2 += 12;
+        }
+
+        long time1InMinutes = hours1 * 60 + minutes1;
+        long time2InMinutes = hours2 * 60 + minutes2;
+
+        long timeDifferenceInMinutes;
+        if (time2InMinutes >= time1InMinutes) {
+            timeDifferenceInMinutes = time2InMinutes - time1InMinutes;
+        } else {
+            timeDifferenceInMinutes = (24 * 60 - time1InMinutes) + time2InMinutes;
+        }
+
+        return timeDifferenceInMinutes / 60;
+    }
+
+
     public static void main(String[] args) throws IOException {
-        String csvFile = "C:\\Users\\disco\\IdeaProjects\\readingCSV\\src\\Assignment_Timecard.xlsx - Sheet1.csv";
+        String csvFile = "src/Repository/Assignment_Timecard.xlsx - Sheet1.csv";
 
         HashMap<String , Employee> map =  new HashMap<>();
         BufferedReader reader = new BufferedReader(new FileReader(csvFile));
@@ -42,9 +94,12 @@ public class Main {
             String name = data[7];
             String pos =  data[1];
             String date =  data[2].split(" ")[0];
-            String timeIn = data[2];
-            String timeOut =  data[3];
+            String timeIn =  data[2] ;
+            String timeOut = data[3];
+
+
             String duration =  data[4];
+
             String arr[] = duration.split(":");
             double workingHours = 0;
             if (arr.length == 2)
@@ -54,12 +109,12 @@ public class Main {
             if(map.containsKey(id))
             {
                 Employee emp =  map.get(id);
-                emp.addWorkDay(date , workingHours);
+                emp.addWorkDay(date , workingHours , timeIn , timeOut);
                 map.put(id ,  emp);
             }
             else {
                 Employee emp =  new Employee(id , name , pos);
-                emp.addWorkDay(date ,workingHours );
+                emp.addWorkDay(date ,workingHours , timeIn , timeOut );
                 map.put(id , emp);
             }
             //System.out.println(id +" " + timeIn + " " + duration +"" +name);
@@ -71,7 +126,6 @@ public class Main {
         {
             Employee emp =  map.get(i);
             List<Workday> workdays = emp.getWorkDays();
-            System.out.println(emp.getId());
             boolean flag =  true;
             for(int j =0 ;j<workdays.size()-1 ;j++)
             {
@@ -88,13 +142,83 @@ public class Main {
 
             if(flag)
             {
-                System.out.println(emp.getId());
                 QuesA.add(emp);
             }
 
         }
 
-        System.out.println(QuesA.size());
+        // finding employee whoes diffrence in sfits timing is more than 1 hr and less than 10 hrs
+
+        List<Employee> QuesB =  new ArrayList<>();
+        List<Employee> QuesC =  new ArrayList<>();
+        for(String i : map.keySet())
+        {
+            Employee emp =  map.get(i);
+            List<Shifts> shift =  emp.getShifts();
+            for(int  j = 0 ;j<shift.size() ;j++)
+            {
+
+                if(shift.get(j).getTimeWorked()> 14)
+                {
+                    QuesC.add(emp);
+                }
+
+            }
+        }
+
+        //Sub SecQuesB
+        for(String i : map.keySet())
+        {
+            Employee emp =  map.get(i);
+            List<Shifts>  shifts =  emp.getShifts();
+
+
+            for(int  j = 0 ;j<shifts.size()-1 ;j++)
+            {
+                String  date1 =  shifts.get(j).getEndTime();
+                String  date2 =  shifts.get(j+1).getStartTime();
+
+
+
+                // Get the difference in hours
+                long hoursDifference = 0;
+                if(date1.length() == 19 && date2.length() == 19) {
+
+                    hoursDifference = manual_hours_diffrence(date1, date2);
+                }
+
+                if(hoursDifference < 10 && hoursDifference > 1)
+                {
+                    QuesB.add(emp);
+                }
+
+            }
+        }
+
+
+        System.out.println("Employees who worked for 7  consecutive days or more ");
+        System.out.println();
+        for(Employee emp : QuesA)
+        {
+            System.out.println(emp.getName() + " " + emp.getPosition());
+        }
+
+        System.out.println("Employee who worked for more than  14 in a single shift ");
+        System.out.println();
+        for(Employee emp : QuesC)
+        {
+            System.out.println(emp.getName() + " " + emp.getPosition());
+        }
+
+        System.out.println("Employee who have less than 10 hours of time between shifts but greater than 1 hour");
+        System.out.println();
+        for(Employee emp : QuesB)
+        {
+            System.out.println(emp.getName() + " " + emp.getPosition());
+        }
+
+
+
 
 
 
